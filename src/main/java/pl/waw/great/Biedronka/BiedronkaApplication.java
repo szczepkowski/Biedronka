f94265e0f2bdb22c1f1b7f4fdb7e0efcc1b7f9c1
pszczepkowski <p.szczepkowski87@gmail.com>
2023-06-05 20:32:02 +0200

    Initial commit

---
+++ src/main/java/pl/waw/great/Biedronka/BiedronkaApplication.java
@@ -0,0 +1,13 @@
+package pl.waw.great.Biedronka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiedronkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiedronkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/waw/great/Biedronka/BiedronkaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.waw.great.Biedronka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiedronkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
